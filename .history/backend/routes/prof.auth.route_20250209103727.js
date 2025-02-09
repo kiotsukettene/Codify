83053fa6d9247cc43af925be425ca5daf97b3ce0
ayrelmay <152473022+ayrelmay@users.noreply.github.com>
2025-02-09 10:54:27 +0800

    unnecessary files deleted

--- .history/backend/routes/prof.auth.route_20250209103727.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { resetPasswordProfessor } from "../controllers/prof.auth.controller.js";
-
-const profrouter = express.Router();
-router.post("/professor/reset-password/:token", resetPasswordProfessor);
-
-export default profrouter;
