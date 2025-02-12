3f3fa06dccaaa27a960f59e9784d63cdec7133ec
ayrelmay <152473022+ayrelmay@users.noreply.github.com>
2025-02-12 22:53:06 +0800

    FE update connection

--- frontend/src/store/profAuthStore.js
+++ frontend/src/store/profAuthStore.js
@@ -233,4 +233,26 @@ export const useprofAuthStore = create((set) => ({
       throw error;
     }
   },
+  resetPassword: async (token, password) => {
+    set({
+      isLoading: true,
+      error: null,
+    });
+
+    try {
+      const response = await axios.post(`${API_URL}/reset-password/${token}`, {
+        password,
+      });
+      set({
+        message: response.data.message,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({
+        isLoading: false,
+        error: error.response.data.message || "Error resetting password",
+      });
+      throw error;
+    }
+  },
 }));
