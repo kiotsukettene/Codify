36fd48b9dfa5e049e1a0dc1368b093dc5ca60500
therinenz <cathybae79@gmail.com>
2025-02-16 23:24:27 +0800

    Changes

--- frontend/src/pages/professor-view-pages/professor-auth/Professor-Login.jsx
+++ frontend/src/pages/professor-view-pages/professor-auth/Professor-Login.jsx
@@ -28,54 +28,6 @@ const ProfessorLogin = () => {
     <div className="relative min-h-screen w-full bg-[#F5EBFF] flex items-center justify-center overflow-hidden p-4">
 
     <ProfBg1 />
-      {/* Background Images */}
-      <div>
-        <img
-          src={logo}
-          alt="Logo"
-          className="absolute top-7 left-24 lg:left-10 lg:w-28"
-        />
-        <img
-          src={pinkFlower}
-          alt="Pink Flower"
-          className="absolute top-16 lg:top-32 left-[-28px] w-16 lg:w-32 opacity-90"
-        />
-        <img
-          src={purpleFlower}
-          alt="Purple Flower"
-          className="absolute bottom-[100px] left-[-30px] w-20 lg:w-52 opacity-90"
-        />
-        <img
-          src={blueFlower}
-          alt="Blue Flower"
-          className="absolute top-[250px] right-[-30px] w-16 lg:w-56 opacity-90"
-        />
-        <img
-          src={union}
-          alt="Star"
-          className="absolute top-10 right-96 w-10 lg:w-52"
-        />
-        <img
-          src={arrow}
-          alt="Arrow"
-          className="absolute top-10 left-24 w-10 lg:w-52"
-        />
-        <img
-          src={arrow}
-          alt="Arrow"
-          className="absolute bottom-20 left-44 w-10 lg:w-52"
-        />
-        <img
-          src={arrow}
-          alt="Arrow"
-          className="absolute bottom-20 right-0 w-20 lg:w-72 rotate-180"
-        />
-        <img
-          src={mascot}
-          alt="Mascot"
-          className="absolute bottom-[-150px] right-[-100px] w-72 lg:w-96"
-        />
-      </div>
 
       {/* Login Card */}
 
