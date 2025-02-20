7f7d8a5d9dbb9939b9ab0bdb518c1e831caa0c55
ayrelmay <152473022+ayrelmay@users.noreply.github.com>
2025-02-20 12:33:35 +0800

    course code length changed to 6

--- backend/utils/generateCourseCode.js
+++ backend/utils/generateCourseCode.js
@@ -1,7 +1,7 @@
 export const generateCourseCode = () => {
   const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   return Array.from(
-    { length: 8 },
+    { length: 6 },
     () => charset[Math.floor(Math.random() * charset.length)]
   ).join("");
 
