49c435e769de59e2e7c34d2ed1aaab1f3c7ddb2d
ayrelmay <152473022+ayrelmay@users.noreply.github.com>
2025-03-09 18:34:31 +0800

    layout

--- frontend/src/Layout/ProfessorLayout.jsx
+++ frontend/src/Layout/ProfessorLayout.jsx
@@ -1,37 +1,37 @@
-// import {
-//   SidebarProvider,
-//   SidebarInset,
-//   SidebarTrigger,
-// } from "@/components/ui/sidebar";
-// import ProfessorSidebar from "@/components/professor-view/Sidebar";
-// import ProfessorSidebar from "@/components/professor-view/Header";
-// import { Outlet } from "react-router-dom";
+import {
+  SidebarProvider,
+  SidebarInset,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import Sidebar from "@/components/professor-view/Sidebar";
+import StudentHeader from "@/components/student-view/Header";
+import { Outlet } from "react-router-dom";
 
-// function ProfessorLayout() {
-//   return (
-//     <SidebarProvider>
-//       <div className="flex h-screen w-full">
-//         {/* ✅ Sidebar */}
-//         <ProfessorSidebar />
+function ProfessorLayout() {
+  return (
+    <SidebarProvider>
+      <div className="flex h-screen w-full">
+        {/* ✅ Sidebar */}
+        <Sidebar />
 
-//         {/* ✅ Main Content */}
-//         <div className="flex flex-col flex-1 w-full">
-//           <SidebarInset className="flex flex-col flex-1 w-full">
-//             {/* ✅ Header */}
-//             <header className="flex h-20 items-center gap-2 px-4 border-b">
-//               <SidebarTrigger className="-ml-1" />
-//               <ProfessorHeader />
-//             </header>
+        {/* ✅ Main Content */}
+        <div className="flex flex-col flex-1 w-full">
+          <SidebarInset className="flex flex-col flex-1 w-full">
+            {/* ✅ Header */}
+            <header className="flex h-20 items-center gap-2 px-4 border-b">
+              <SidebarTrigger className="-ml-1" />
+              <StudentHeader />
+            </header>
 
-//             {/* ✅ Page Content */}
-//             <div className="flex flex-1 w-full  mx-auto overflow-auto bg-neutral-50 p-4 scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
-//               <Outlet />
-//             </div>
-//           </SidebarInset>
-//         </div>
-//       </div>
-//     </SidebarProvider>
-//   );
-// }
+            {/* ✅ Page Content */}
+            <div className="flex flex-1 w-full  mx-auto overflow-auto bg-neutral-50 p-4 scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
+              <Outlet />
+            </div>
+          </SidebarInset>
+        </div>
+      </div>
+    </SidebarProvider>
+  );
+}
 
-// export default ProfessorLayout;
+export default ProfessorLayout;
