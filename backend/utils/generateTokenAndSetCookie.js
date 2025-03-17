0cc869605fc30d72c25fdd9cd36b1697586a0a23
sengyyy <kiotsuketteneloreto@gmail.com>
2025-03-18 01:16:12 +0800

    Updated the token to handle production

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -10,7 +10,7 @@ export const generateTokenAndSetCookie = (res, institutionId) => {
     res.cookie("token", token, {
         httpOnly: true, // means cannot be accesible via javascript, only http
         secure: process.env.NODE_ENV === 'production', // only works in https
-        sameSite: "strict",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     })
 
