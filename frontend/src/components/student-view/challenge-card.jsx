a4debda940afe768cafc362ce6407fb514c0be8d
Cel3stt <mariannecelest@gmail.com>
2025-04-08 22:03:01 +0800

    fix height and responsiveness

--- frontend/src/components/student-view/challenge-card.jsx
+++ frontend/src/components/student-view/challenge-card.jsx
@@ -24,7 +24,7 @@ const StudentChallengeCard = ({ id, title, description, tags = [], status = 'pen
   }
 
   return (
-    <Card className="w-full h-auto mx-auto bg-white rounded-xl shadow-sm border-none overflow-hidden transition-all duration-300 dark:bg-gray-950 p-5">
+    <Card className="w-full h-[23rem] mx-auto bg-white rounded-xl shadow-sm border-none overflow-hidden transition-all duration-300 dark:bg-gray-950 p-5">
     {/* Image Section */}
     <div className="relative bg-[#EEF7FE] rounded-lg">
       <img
@@ -49,10 +49,10 @@ const StudentChallengeCard = ({ id, title, description, tags = [], status = 'pen
       {/* Course Details */}
       <CardContent className="mt-6 space-y-2 p-0 flex-grow">
         <h3 className="text-lg font-medium text-[#415EB6]">{title}</h3>
-        <h4 className="text-sm text-neutral-900 dark:text-neutral-200">{description}</h4>
+        <h4 className="text-sm overflow-hidden text-ellipsis line-clamp-2 max-h-12 text-neutral-900 dark:text-neutral-200">{description}</h4>
       </CardContent>
 
-      <CardFooter className="p-0 mt-3 justify-end">
+      <CardFooter className="p-0 mt-5 justify-end">
         <Button variant="secondary" className={`${buttonStatus[status]} transition-colors`} onClick={handleStartChallenge}>
           {status === "completed" ? "Completed" : "Start Practice"}
         </Button>
