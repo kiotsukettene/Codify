083148c8935f4d6347ce1dfa4801c5e5b152336a
Cel3stt <mariannecelest@gmail.com>
2025-04-16 22:21:00 +0800

    change vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import dotenv from "dotenv";
 
-dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env": process.env,
   },
   resolve: {
     alias: {
