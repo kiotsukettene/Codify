5da6417a341f12f18f2c0bfdf69b16f5da9fa9bd
ayrelmay <152473022+ayrelmay@users.noreply.github.com>
2025-04-19 05:13:56 +0800

    modela and route

--- backend/routes/course.route.js
+++ backend/routes/course.route.js
@@ -5,13 +5,22 @@ import {
   getCourseById,
   updateCourse,
   deleteCourse,
+  getCoursesByProfessor,
+  getUniqueStudentCountByProfessor,
 } from "../controllers/course.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
+import { profVerifyToken } from "../middleware/professorVerifyToken.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createCourse);
 router.get("/courses", verifyToken, getCoursesByInstitution);
+router.get("/professor/courses", profVerifyToken, getCoursesByProfessor);
+router.get(
+  "/professor/unique-student-count",
+  profVerifyToken,
+  getUniqueStudentCountByProfessor
+);
 router.get("/course/:courseId", getCourseById);
 router.put("/update/:courseId", updateCourse);
 router.delete("/delete/:courseId", deleteCourse);
