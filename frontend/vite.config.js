4c104e338e6abfc2f31d39abf28e048b782b1024
kiotsukettene <kiotsuketteneloreto@gmail.com>
2025-04-26 10:01:35 +0800

    try

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,20 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      external: ['socket.io-client']
-    }
-  }
 });
