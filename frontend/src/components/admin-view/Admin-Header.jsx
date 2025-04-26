2430d590899cb8df4808c9ee178f5d6922f7fed0
kiotsukettene <kiotsuketteneloreto@gmail.com>
2025-04-26 16:25:36 +0800

    production

--- frontend/src/components/admin-view/Admin-Header.jsx
+++ frontend/src/components/admin-view/Admin-Header.jsx
@@ -45,20 +45,7 @@ function AdminHeader() {
         className="flex gap-3 items-center md:flex md:items-center z-[-1] md:z-auto md:static 
       absolute top-0 right-0 p-4 md:space-x-4 lg:space-x-0"
       >
-        {/* Notifications */}
-        <div>
-          <Button variant="outline" size="icon" className="h-8 w-8 relative">
-            <BellRing className="h-4 w-4" />
-            <span className="absolute -top-2 -right-2 h-5 w-5 rounded-full bg-destructive text-xs text-destructive-foreground flex items-center justify-center">
-              1
-            </span>
-          </Button>
-        </div>
-
-        {/* Achievement Badge */}
-        <div>
-          <img className="w-8 h-8" src={badge} alt="Badge" />
-        </div>
+       
 
         {/* Avatar with Dropdown */}
         <div className="relative" ref={dropdownRef}>
